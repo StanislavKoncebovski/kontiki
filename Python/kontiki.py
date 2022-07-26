b47627530ea0b798d1a86cc8ec879b920251963e
Stanislav <stanislav@pikkatech.eu>
2022-07-26 14:10:03 +0200

    Class Kontiki: further definitions

--- Python/kontiki.py
+++ Python/kontiki.py
@@ -1,10 +1,11 @@
+from typing import Union
 import pandas as pd
 
 class Kontiki:
     '''
     Contains functions to query and retrieve books and articles from a LibGen server.
     '''
-    def query_books(self, tokens: str) -> pd.Dataframe:
+    def query_books(self, tokens: str) -> pd.DataFrame:
         '''
         Carries out a general query for books using common tokens like title and author(s) name(s).
         :param tokens: The string containing the tokens. The tokens can be divided by space or comma.
@@ -13,7 +14,7 @@ class Kontiki:
         '''
         pass
 
-    def query_books_by_isbn(self, isbn: str) -> pd.Dataframe:
+    def query_books_by_isbn(self, isbn: str) -> pd.DataFrame:
         '''
         Carries out a query for a book by ISBN.
         :param isbn: The ISBN string. Can contain more than one ISBN (e.g. the 10-digit and the 13-digit ISBN), divided
@@ -22,7 +23,7 @@ class Kontiki:
         '''
         pass
 
-    def query_books_by_tags(self, tags: str):
+    def query_books_by_tags(self, tags: str) -> pd.DataFrame:
         '''
         Carries out a query for books by tags.
         :param tags: The tags (e.g. "mathematics, algebra") divided by a space or comma.
@@ -30,18 +31,19 @@ class Kontiki:
         '''
         pass
 
-    def retrieve_book(self, md5: str, format: str = 'dict'):
+    def retrieve_book(self, md5: str, format: str = 'dict') -> Union[dict[str, str], str, None]:
         '''
         Retrieves a book by its unique MD5 value.
         :param md5: The MD5 code of the book.
         :param format: Defines the format of the output. If it is 'dict' (default) the result is a dictionary containing
                        the fields, such as MD5, Title, Author, etc.
                        If it is 'bibtex' the result is a BibTeX reference of the book.
-        :return: The dictionary of the book's fields or its BibTeX reference, depending on the output format defined.
+        :return: If found, the dictionary of the book's fields or its BibTeX reference,
+                 depending on the output format defined. None, if not found.
         '''
         pass
 
-    def query_articles(self, tokens: str) -> pd.Dataframe:
+    def query_articles(self, tokens: str) -> pd.DataFrame:
         '''
         Carries out a general query for articles using common allowed tokens such as title, author(s), and DOI.
         :param tokens: The string containing the tokens. The tokens can be divided by space or comma.
@@ -50,13 +52,14 @@ class Kontiki:
         '''
         pass
 
-    def retrieve_article(self, doi: str, format: str = 'dict'):
+    def retrieve_article(self, doi: str, format: str = 'dict') -> Union[dict[str, str], str, None]:
         '''
         Retrieves an article by its DOI value.
         :param doi: The DOI value.
         :param format: Defines the format of the output. If it is 'dict' (default) the result is a dictionary containing
                        the fields, such as MD5, Title, Author, DOI, etc.
                        If it is 'bibtex' the result is a BibTeX reference of the article.
-        :return: The dictionary of the article's fields or its BibTeX reference, depending on the output format defined.
+        :return: If found, the dictionary of the article's fields or its BibTeX reference,
+                 depending on the output format defined. None, if not found.
         '''
         pass
