9b01e103e5e342e7d97ade5e33b2a37ffcd8b809
Stanislav <stanislav@pikkatech.eu>
2022-08-17 23:27:23 +0200

    Data\baseUrls.xml

--- CSharp/Kontiki.UnitTests/KontikiBaseUrlManagerTests.cs
+++ CSharp/Kontiki.UnitTests/KontikiBaseUrlManagerTests.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿/***********************************************************************************
+* File:         KontikiBaseUrlManagerTests.cs                                      *
+* Contents:     Class KontikiBaseUrlManagerTests                                   *
+* Author:       Stanislav Koncebovski (stanislav@pikkatech.eu)                     *
+* Date:         2022-08-17 23:08                                                   *
+* Version:      1.0                                                                *
+* Copyright:    pikkatech.eu (www.pikkatech.eu)                                    *
+***********************************************************************************/
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace Kontiki.UnitTests
@@ -34,6 +39,41 @@ namespace Kontiki.UnitTests
 			manager.TestBaseUrl(baseUrl);
 
 			Assert.That(!baseUrl.IsOnline);
+		}
+
+		[Test]
+		public void ConversionToXmlSucceeds()
+		{
+			KontikiBaseUrlManager manager = new KontikiBaseUrlManager();
+			//KontikiBaseUrl baseUrl = new KontikiBaseUrl("default", "http://libgen.rs/");
+			//manager.TestBaseUrl(baseUrl);
+
+			//manager.AddBaseUrl(baseUrl);
+
+			//manager.Save("baseUrls.xml");
+
+
+			Dictionary<string, string>	baseUrls = new Dictionary<string, string>()
+			{
+				{"Default", "http://libgen.rs/"},
+				{"Proxy01", "https://freeanimesonline.com/proxify.php?proxy=ZnJlZWFuaW1lc29ubGluZS5jb20=&amp;site=aHR0cDovL2dlbi5saWIucnVzLmVjLw=="},
+				{"Proxy02",	"https://sitenable.info/proxify.php?proxy=c2l0ZW5hYmxlLmluZm8=&amp;site=aHR0cDovL2dlbi5saWIucnVzLmVjLw=="},
+				{"Proxy03", "https://siteget.net/proxify.php?proxy=c2l0ZWdldC5uZXQ=&amp;site=aHR0cDovL2dlbi5saWIucnVzLmVjLw=="},
+				{"Proxy04", "https://freeproxy.io/proxify.php?proxy=ZnJlZXByb3h5Lmlv&amp;site=aHR0cDovL2dlbi5saWIucnVzLmVjLw=="},
+				{"Proxy05", "https://sitenable.co/proxify.php?proxy=c2l0ZW5hYmxlLmNv&amp;site=aHR0cDovL2dlbi5saWIucnVzLmVjLw=="},
+				{"Proxy06", "https://sitenable.ch/proxify.php?proxy=c2l0ZW5hYmxlLmNo&amp;site=aHR0cDovL2dlbi5saWIucnVzLmVjLw=="},
+				{"Proxy07", "https://sitenable.pw/proxify.php?proxy=c2l0ZW5hYmxlLnB3&amp;site=aHR0cDovL2dlbi5saWIucnVzLmVjLw=="},
+			};
+
+			foreach (string name in baseUrls.Keys)
+			{
+				KontikiBaseUrl baseUrl = new KontikiBaseUrl(name, baseUrls[name]);
+				manager.TestBaseUrl(baseUrl);
+
+				manager.AddBaseUrl(baseUrl);
+			}
+
+			manager.Save("baseUrls.xml");
 		}
 	}
 }
